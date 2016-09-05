c353042ce652418965dbb53946128ebe677b9bf1
SanyaDooM <sanyadoom@yandex.ru>
2016-09-06 02:07:20 +0300

    ver 4

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+using namespace std;
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	
 	std::cout<<"Saniok Yo!"<<std::endl;
+	int x;
+	cout<<"Press buttton key"<<endl;
+	cin>>x;
 	
 	system("pause");
 	return 0;
